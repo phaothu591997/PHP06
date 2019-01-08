072091e88f6eb4070c63ef8a7e4f779373e2d0c9
Phao Thu <phaothu591997@gmail.com>
2019-01-08 19:19:28 +0700

    oppex

---
+++ opp/example1.php
@@ -0,0 +1,47 @@
+<?php
+	class User{
+		var $name;
+		var $email;
+		var $password;
+		var $phone;
+		var $addrees;
+		private function addUser(){
+			echo "add";
+		}
+		function editUser(){
+			echo "edit";
+		}
+		function registerUser(){
+			echo "register";
+		}
+		function viewUser(){
+			echo "view";
+		}
+		private function listUser(){
+			echo "list";
+		}
+	}
+
+
+	class Customer extends User{
+		var $name;
+		var $email;
+		var $password;
+		var $phone;
+		var $addrees;
+		var $addreesGh;
+		var $idKH;
+		function payCustomer(){
+			echo "pay";
+		}
+		function politeCustomer(){
+			echo "polite";
+		}
+		
+	}
+	$user = new User();
+	$user->registerUser();
+	echo "<br>";
+	$customer = new Customer();
+	$customer ->payCustomer();
+ ?>
